fa9d11e8a19a881ea897896cd6ed8ea4a2b17154
mechack tambwe <mesutarso@users.noreply.github.com>
2021-05-31 23:03:20 +0100

    feat: add ex1 ex2 ex3 ex4

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tournois des génies | LWEB2</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-light bg-dark text-light">
+  <a class="navbar-brand d-flex justify-content-center" href="#">
+    <h2 class="text-light "> Tournois de genies 1</h2>
+  </a>
+</nav>
+    </header>
+    <main>
+      <div class="container py-5">
+        <div class="row">
+        <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Exercice 1</h5>
+               <p class="card-text">VARIABLE</p>
+              <a href="./exercices/exercice1/index.php" class="btn btn-primary">Voir la Solution</a>
+              </div>
+       </div>
+       
+       <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Exercice 2</h5>
+               <p class="card-text">LES TESTS </p>
+              <a href="./exercices/exercice2/test.php" class="btn btn-primary">Voir la Solution</a>
+              </div>
+       </div>
+       <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Exercice 3</h5>
+               <p class="card-text">LA BOUCLE WHILE</p>
+              <a href="./exercices/exercice3/while.php" class="btn btn-primary">Voir la Solution</a>
+              </div>
+       </div>
+       <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Exercice 4</h5>
+               <p class="card-text">LA BOUCLE FOR</p>
+              <a href="./exercices/exercice4/for.php" class="btn btn-primary">Voir la Solution</a>
+              </div>
+       </div>
+       <div class="card col-3 mx-3 my-3" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Exercice 4.1</h5>
+               <p class="card-text">LA BOUCLE FOR, WHILE, FOREACH</p>
+              <a href="./exercices/exercice3/loop.php" class="btn btn-primary">Voir la Solution</a>
+              </div>
+       </div>
+      </div>
+    </main>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
